e4651b22315dc12b24e3449718c26d361b2c4878
knguyen111601 <knguyen111601@gmail.com>
2022-06-22 12:53:49 -0700

    beginning useReducer

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from "react-router-dom"
 import StateTutorial from "./StateTutorial"
 import Home from "./Home"
+import ReducerTutorial from "./ReducerTutorial"
 
 const Main = () => {
     return <>
@@ -8,6 +9,7 @@ const Main = () => {
         <Route path="*" element={<h1>Page does not exist or hasn't been created yet.</h1>}/>
         <Route path="/" element={<Home/>}/>
         <Route path="/useState" element={<StateTutorial/>}/>
+        <Route path="/useReducer" element={<ReducerTutorial/>}/>
     </Routes>
     </>
 
---
+++ src/pages/ReducerTutorial.js
@@ -0,0 +1,10 @@
+const ReducerTutorial = () => {
+    return (<div>
+
+        <h1>useReducer</h1>
+
+
+    </div>)
+}
+
+export default ReducerTutorial
