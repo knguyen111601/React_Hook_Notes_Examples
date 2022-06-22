47f53ab4a555558880ab941d58dbd6844af7a3d3
knguyen111601 <knguyen111601@gmail.com>
2022-06-21 21:30:50 -0700

    have links to all hooks mapped on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
+
+    const hooks = ["useState", "useReducer", "useEffect", "useRef", "useLayoutEffect", "useImperativeHandle", "useContext", "useMemo", "useCallback"]
+
+    const renderLinks = () => {
+        return hooks.map((hook)=>{
+            return <Link to={`/${hook}`}>
+                <h1>{hook}</h1>
+            </Link>
+        })
+    }
+
     return <>
         <h1>Home</h1>
+        {renderLinks()}
     </>
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import Home from "./Home"
 const Main = () => {
     return <>
     <Routes>
+        <Route path="*" element={<h1>Page does not exist or hasn't been created yet.</h1>}/>
         <Route path="/" element={<Home/>}/>
         <Route path="/useState" element={<StateTutorial/>}/>
     </Routes>
