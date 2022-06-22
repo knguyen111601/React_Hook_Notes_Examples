b876944c38a6a3c9e520ad022e3dcccb5e9cd7bf
knguyen111601 <knguyen111601@gmail.com>
2022-06-22 13:20:39 -0700

    useState info added

--- src/pages/StateTutorial.js
+++ src/pages/StateTutorial.js
@@ -3,8 +3,7 @@ import { useState } from "react"
 const StateTutorial = () => {
     const style = {
         margin: "auto",
-        width: "80%",
-        marginTop:"100px"
+        width: "80%"
     }
 
     const [form, setForm] = useState({username: "", email: ""})
@@ -15,7 +14,14 @@ const StateTutorial = () => {
         setForm(newForm)
     }
 
-    return <div style={style}>
+    return <>
+    <div style={{borderBottom:"2px dotted black"}}>
+        <h1 style={{textAlign:"center", fontSize:"3em"}}>useState Example</h1>
+        <p>Here, we have the form with a handleChange function that updates state on each change</p>
+        <p>The inital state of this form is an object with keys, username and email that are set to empty string values</p>
+        <p>Upon each change, the handleChange function is setting the state to a new state with items in the form</p>
+    </div>
+    <div style={style}>
             <h1>Current Username State: <span style={{color:"red"}}>{form.username}</span></h1>
             <h1>Current E-Mail State: <span style={{color:"red"}}>{form.email}</span></h1>
         <div style={style}>
@@ -37,6 +43,7 @@ const StateTutorial = () => {
         </form>
         </div>
     </div>
+    </>
 }
 
 export default StateTutorial
