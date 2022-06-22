564c136cb43333d5f3f90668c5cc929118d9cdf2
knguyen111601 <knguyen111601@gmail.com>
2022-06-22 13:07:56 -0700

    useReducer example complete

--- src/pages/ReducerTutorial.js
+++ src/pages/ReducerTutorial.js
@@ -1,8 +1,35 @@
+import { useReducer } from "react"
+
 const ReducerTutorial = () => {
+    const reducer = (state, action) => {
+        switch(action.type) {
+            case "INCREMENT": 
+                return {count: state.count+1, showText: state.showText}
+    
+            case "toggleShowText":
+                return {count: state.count, showText: !state.showText}
+    
+            default:
+                return state
+        }
+    }
+
+    // state holds the states
+    // dispatch passes the type to the reducer function to update certain states
+    const [state, dispatch] = useReducer(reducer, {count:0, showText:true})
+
     return (<div>
 
-        <h1>useReducer</h1>
+        <h1>{state.count}</h1>
+        <button onClick={()=>{
+            dispatch({type:"INCREMENT"})
+            dispatch({type:"toggleShowText"})
+        }}>
+            Click Here
+        </button>
 
+        {/* If showText is true, <p></p> will render*/}
+        {state.showText && <p>Text appears</p>}
 
     </div>)
 }
